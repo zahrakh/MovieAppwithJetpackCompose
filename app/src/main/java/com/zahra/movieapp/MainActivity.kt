9854e63d396adf6fe1c45c4f7b5af0f19ef8971e
zahra <zahra.khodabakhshi527@gmail.com>
2023-05-25 10:55:26 +0330

    Apply spotless to entire code

--- app/src/main/java/com/zahra/movieapp/MainActivity.kt
+++ app/src/main/java/com/zahra/movieapp/MainActivity.kt
@@ -13,31 +13,31 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.zahra.movieapp.ui.theme.MovieAppTheme
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            MovieAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+      MovieAppTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+          modifier = Modifier.fillMaxSize(),
+          color = MaterialTheme.colors.background,
+        ) {
+          Greeting("Android")
         }
+      }
     }
+  }
 }
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+  Text(text = "Hello $name!")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    MovieAppTheme {
-        Greeting("Android")
-    }
+  MovieAppTheme {
+    Greeting("Android")
+  }
 }
